91a330d8bf1035997295636c37152a7da4b3e16b
Carlos Sempe <sempejunior@gmail.com>
2022-10-19 18:23:29 -0300

    Created Sidebar

--- project-01/src/App.jsx
+++ project-01/src/App.jsx
@@ -1,13 +1,24 @@
 import { Header } from "./components/Header";
 import { Post } from "./Post";
+import styles from "./App.module.css";
 
 import "./global.css";
+import { Sidebar } from "./components/Sidebar";
 
 export function App() {
   return (
     <>
       <Header />
-      <Post author="Carlos" content="Lorem ipsum" />
+
+      <div className={styles.wrapper}>
+        <Sidebar />
+        <main>
+          <Post
+            author="Carlos"
+            content="Lorem ipsumfdsfdsgfdsfgdsg shnfjksdhfnjikdghbhjskgbhkjsgb jdfklsfnjdskfjskl"
+          />
+        </main>
+      </div>
     </>
   );
 }
---
+++ project-01/src/components/Sidebar.jsx
@@ -0,0 +1,20 @@
+import styles from "./Sidebar.module.css";
+
+export function Sidebar() {
+  return (
+    <aside className={styles.sidebar}>
+      <img
+        className={styles.cover}
+        src="https://images.unsplash.com/photo-1534972195531-d756b9bfa9f2?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=60"
+      />
+      <div className={styles.profile}>
+        <strong>Carlos Sempé</strong>
+        <span>Development Coordinator</span>
+      </div>
+
+      <footer>
+        <a href="#">Editar Seu Perfil</a>
+      </footer>
+    </aside>
+  );
+}
