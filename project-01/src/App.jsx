00b8c800ce9a96d926f0b65917780897a3be5bf1
Carlos Sempe <sempejunior@gmail.com>
2022-10-19 17:19:08 -0300

    Created Header Component

--- project-01/src/App.jsx
+++ project-01/src/App.jsx
@@ -1,10 +1,12 @@
+import { Header } from "./components/Header";
 import { Post } from "./Post";
 
-import "./styles.css";
+import "./global.css";
 
 export function App() {
   return (
     <>
+      <Header />
       <Post author="Carlos" content="Lorem ipsum" />
     </>
   );
--- project-01/src/components/Header.jsx
+++ project-01/src/components/Header.jsx
@@ -1,7 +1,12 @@
+import styles from "./Header.module.css";
+import igniteLogo from "../assets/igniteLogo.png";
+
+console.log(styles);
+
 export function Header() {
   return (
-    <strong>
-      Ignite <Feed></Feed>
-    </strong>
+    <header className={styles.header}>
+      <img src={igniteLogo} alt="Logotipo do Ignite" />
+    </header>
   );
 }
