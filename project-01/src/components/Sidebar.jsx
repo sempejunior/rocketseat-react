6196b844f8aa36a36b01e7f43f73a7faeb45fc4d
Carlos Sempe <sempejunior@gmail.com>
2022-10-20 08:09:02 -0300

    Created Sidebar component

--- project-01/src/components/Sidebar.jsx
+++ project-01/src/components/Sidebar.jsx
@@ -1,4 +1,5 @@
 import styles from "./Sidebar.module.css";
+import { PencilLine } from "phosphor-react";
 
 export function Sidebar() {
   return (
@@ -8,12 +9,16 @@ export function Sidebar() {
         src="https://images.unsplash.com/photo-1534972195531-d756b9bfa9f2?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=60"
       />
       <div className={styles.profile}>
+        <img className={styles.avatar} src="https://github.com/sempejunior.png" />
         <strong>Carlos Sempé</strong>
         <span>Development Coordinator</span>
       </div>
 
       <footer>
-        <a href="#">Editar Seu Perfil</a>
+        <a href="#">
+          <PencilLine size={20} />
+          Editar Seu Perfil
+        </a>
       </footer>
     </aside>
   );
